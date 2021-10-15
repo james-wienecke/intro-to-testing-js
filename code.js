807a163ea9da8929e2798460cd38a55e16e5cdb0
James Wienecke <james.a.wienecke@gmail.com>
2021-10-15 11:07:06 -0500

    sayHello() refactor complete, solution generalized.

--- code.js
+++ code.js
@@ -6,11 +6,5 @@ const helloWorld = function() {
 // sayHello function
 // TODO: return string of hello + any input passed
 function sayHello(input) {
-    if (input === "Alex") {
-        return "Hello, Alex!";
-    } else if (input === "Pat") {
-        return "Hello, Pat!";
-    }   else {
-        return "Hello, Jane!";
-    }
+    return "Hello, " + input + "!";
 }
